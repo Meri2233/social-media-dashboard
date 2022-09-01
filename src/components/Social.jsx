6f2b5d23520edfb60246d18dec1ee3e533f0b689
Meri2233 <sharmamerika2@gmail.com>
2022-09-01 15:05:27 +0545

    upadated the alt

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -5,7 +5,7 @@ function Social({name,followers,status,today,bgColor,image,icon,txtColor}) {
             <p className="name gray"> <img src={image}></img> {name}</p>
             <h1 className="followers ">{followers}</h1>
             <p className="tag gray">{status}</p>
-            <p className="today" style={{color:txtColor}}> <img src={icon} alt="" /> {today}</p>
+            <p className="today" style={{color:txtColor}}> <img src={icon} alt="icon" /> {today}</p>
         </div>
     )
 }
