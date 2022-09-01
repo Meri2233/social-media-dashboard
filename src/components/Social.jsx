f9de11d216701e79fe1b61c9430ddca9576fe681
Meri2233 <sharmamerika2@gmail.com>
2022-09-01 15:07:21 +0545

    updated alt

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -2,7 +2,7 @@
 function Social({name,followers,status,today,bgColor,image,icon,txtColor}) {
     return (
         <div style={{backgroundColor:bgColor}} className="scard">
-            <p className="name gray"> <img src={image}></img> {name}</p>
+            <p className="name gray"> <img src={image} alt=""></img> {name}</p>
             <h1 className="followers ">{followers}</h1>
             <p className="tag gray">{status}</p>
             <p className="today" style={{color:txtColor}}> <img src={icon} alt="icon" /> {today}</p>
